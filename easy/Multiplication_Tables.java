47dd94ec3c9e49f9f0ccefe288cc6567a7928b8a
jjerge <jerge92@comcast.net>
2017-08-10 09:16:04 -0400

    Create Multiplication_Tables.java

---
+++ easy/Multiplication_Tables.java
@@ -0,0 +1,19 @@
+package build;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		    for(int x=1;x<=12;x++){
+		    	for(int y=1;y<=12;y++){
+		    		int temp=x*y;
+		    		System.out.printf("%4d",temp);
+		    	}
+		    	System.out.println();
+		    }
+		}
+}
+
+		
